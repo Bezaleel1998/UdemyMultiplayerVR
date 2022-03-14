b6a095ddbcd4c9f1d18d6512adcb22d362e97d76
NightHawk1998 <bezaleelairbornedivision@yahoo.com>
2022-03-14 16:40:52 +0700

    VR Development Basics
    - avatar Implementation
    - Continuous Turn, Moving and Character Controller
    - teleportation
    - grabbing
    - UI interaction

---
+++ UdemyMultiplayerTemplate/Assets/Scripts/ButtonInteractionDummy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteractionDummy : MonoBehaviour
+{
+    
+    public void OnButtonPressed()
+    {
+
+        Debug.Log("UI Button Pressed");
+
+    }
+
+}
